799ab6b8304eb8e56c7403e5116a38bb52ea32d3
Cristian Villa <c.villavirgen@gmail.com>
2023-06-26 14:40:05 -0600

    Add mobile-menu imgs

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-document.getElementsByTagName('h1').innerHtml = 'From JS';
+// Select elements
+var hamburgerIcon = document.getElementById("hamburger-icon");
+var closeIcon = document.getElementById("close-icon");
+var mobileMenu = document.getElementById("mobile-menu");
+var menuLinks = mobileMenu.getElementsByTagName("a");
+
+// Function to show mobile menu
+function showMobileMenu() {
+  mobileMenu.classList.add("visible");
+}
+
+// Function to hide mobile menu
+function hideMobileMenu() {
+  mobileMenu.classList.remove("visible");
+}
+
+// Add event listeners
+hamburgerIcon.addEventListener("click", showMobileMenu);
+closeIcon.addEventListener("click", hideMobileMenu);
+
+// Add event listeners to menu links
+for (var i = 0; i < menuLinks.length; i++) {
+  menuLinks[i].addEventListener("click", hideMobileMenu);
+}
