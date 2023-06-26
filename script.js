e429bb488d1c6c3f57cc1c5d0866909a021bfb52
Cristian Villa <c.villavirgen@gmail.com>
2023-06-26 15:44:35 -0600

    Solve ESLint issues

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 // Select elements
-var hamburgerIcon = document.getElementById("hamburger-icon");
-var closeIcon = document.getElementById("close-icon");
-var mobileMenu = document.getElementById("mobile-menu");
-var menuLinks = mobileMenu.getElementsByTagName("a");
+const hamburgerIcon = document.getElementById('hamburger-icon');
+const closeIcon = document.getElementById('close-icon');
+const mobileMenu = document.getElementById('mobile-menu');
+const menuLinks = mobileMenu.getElementsByTagName('a');
 
 // Function to show mobile menu
 function showMobileMenu() {
-  mobileMenu.classList.add("visible");
+  mobileMenu.classList.add('visible');
 }
 
 // Function to hide mobile menu
 function hideMobileMenu() {
-  mobileMenu.classList.remove("visible");
+  mobileMenu.classList.remove('visible');
 }
 
 // Add event listeners
-hamburgerIcon.addEventListener("click", showMobileMenu);
-closeIcon.addEventListener("click", hideMobileMenu);
+hamburgerIcon.addEventListener('click', showMobileMenu);
+closeIcon.addEventListener('click', hideMobileMenu);
 
 // Add event listeners to menu links
-for (var i = 0; i < menuLinks.length; i++) {
-  menuLinks[i].addEventListener("click", hideMobileMenu);
+for (let i = 0; i < menuLinks.length; i += 1) {
+  menuLinks[i].addEventListener('click', hideMobileMenu);
 }
